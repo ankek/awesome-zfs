2bc72c4a87342208596fb032944e243db7e46733
ankek <you@example.com>
2025-09-07 14:50:16 +0300

    comment out file

--- test-scenarios/1_preparations.sh
+++ test-scenarios/1_preparations.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# This script
+# 1. Creates a temporary directory /scratch to hold test files.
+# 2. Makes it fully accessible to all users.
+# 3. Generates 12 empty files with size large 3 GB in directory /scratch, which can be used for subsequent testing 
+# or experimentation with ZFS (e.g., creating a ZFS pool, testing snapshots, or simulating large disk usage).
+
 sudo mkdir /scratch
 sudo chmod 777 /scratch/
 for i in {1..12}; do truncate -s 3G /scratch/3g-$i.img; done
--- test-scenarios/2_1_zfs_single.sh
+++ test-scenarios/2_1_zfs_single.sh
@@ -1,5 +1,16 @@
-# Please run as root
-############
+#!/bin/bash
+# This script
+# 1. Create a single-drive ZFS pool using a 3 GB test file as a virtual disk.
+# 2. Inspect the pool and its datasets with zfs list.
+# 3. Check pool status to ensure it is online and error-free.
+# 4. Review pool statistics with zpool list.
+# 5. Monitor I/O activity with zpool iostat.
+# 6. Clean up by destroying the test pool after inspection.
+
+########################
+## Please run as root ##
+########################
+
 echo "###################################"
 echo "ZFS Single Drive Pool"
 echo "Number of Drives - 1"
